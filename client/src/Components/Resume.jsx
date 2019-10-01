26d3d6e72786b53fd54c2b78caaa5163bee391d7
Michael Thompson <michael.220488@gmail.com>
2019-10-01 12:54:44 -0700

    issue with method in subcompnent (not defined)

--- client/src/Components/Resume.jsx
+++ client/src/Components/Resume.jsx
@@ -1,19 +1,48 @@
 import React from 'react'
+import ResumeText from './ResumeText.jsx'
 
-const Resume = () => {
-  return (
-    <div>
-      <div className="title">
-        <h2>Résumé</h2>
+class Resume extends React.Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      text: ''
+    }
+    this.state.handleClickz= this.state.bind(handleClickz)
+  }
+
+  handleClickz(e) {
+    const id = e.target.value
+    const storage = {
+      skills: "hi",
+      apps: "how",
+      experience: "are",
+      education: "you"
+    }
+    this.setState({
+      text: storage[id]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="title">
+          <h2>Résumé</h2>
+        </div>
+        <div className="resume-buttons-container">
+          <button className="resume-buttons" value="skills" id="technical-skill-button" onClick={this.handleClickz}> Technical Skills</button>
+          <button className="resume-buttons" value="apps" id="software-engineering-application-button" onClick={this.handleClickz}> Software Engineering Applications</button>
+          <button className="resume-buttons" value="experience" id="professional-experience-button" onClick={this.handleClickz}> Professional Experience</button>
+          <button className="resume-buttons" value="education" id="education-button" onClick={this.handleClickz}> Education</button>
+        </div>
+        <div className="body-text">
+        {this.state.text.length &&
+          <ResumeText text={this.state.text}/>
+        }
+        </div>
       </div>
-      <div className="resume-buttons-container">
-        <button className="resume-buttons" id="technical-skill-button"> Technical Skills</button>
-        <button className="resume-buttons" id="software-engineering-application-button"> Software Engineering Applications</button>
-        <button className="resume-buttons" id="professional-experience-button"> Professional Experience</button>
-        <button className="resume-buttons" id="education-button"> Education</button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Resume
