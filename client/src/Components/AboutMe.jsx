057fbfbdbfcbace1e82a41c3705b229178109d17
Michael Thompson <michael.220488@gmail.com>
2019-11-01 15:52:57 -0700

    ammended webpack loaders to allow for images

--- client/src/Components/AboutMe.jsx
+++ client/src/Components/AboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import hikingPhoto from '../../dist/hiking.jpg'
 const AboutMe = () => {
   return (
     <div>
@@ -9,8 +9,10 @@ const AboutMe = () => {
       <div className="body-text">
         I was born and raised in Seattle, Washington. I love learning about new things and coding has become one of my passions. In addition, I love learning about the world, playing sports, and enjoying the outdoors.
       </div>
-      <div>
-        <img ></img>
+      <div className="image-container">
+        <img className="image" src={hikingPhoto}/>
+        <img/>
+        <img/>
       </div>
     </div>
   )
