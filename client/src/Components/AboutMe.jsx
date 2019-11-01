9cd262e3e977cfde62e42a158164a2e534e5ecaa
Michael Thompson <michael.220488@gmail.com>
2019-11-01 14:43:29 -0700

    beginning to add images to about me section

--- client/src/Components/AboutMe.jsx
+++ client/src/Components/AboutMe.jsx
@@ -6,7 +6,12 @@ const AboutMe = () => {
       <div className="title">
         <h2>A Little About Me:</h2>
       </div>
-      <div className="body-text">I was born and raised in Seattle, Washington. I love learning about new things and coding has become one of my passions. In addition, I love learning about the world, playing sports, and enjoying the outdoors.</div>
+      <div className="body-text">
+        I was born and raised in Seattle, Washington. I love learning about new things and coding has become one of my passions. In addition, I love learning about the world, playing sports, and enjoying the outdoors.
+      </div>
+      <div>
+        <img ></img>
+      </div>
     </div>
   )
 }
