93fe461c196255bb6f7bcbf9deb714db2cd26a80
Michael Thompson <michael.220488@gmail.com>
2019-11-21 14:00:34 -0800

    added to my projects page

--- client/src/Components/MyProjects.jsx
+++ client/src/Components/MyProjects.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import rural from '../../dist/ruraloutfitters.jpg'
+import cloudBNB  from '../../dist/Nginxcode.png'
 // Consider making this into a carousel that shows user-side pics and/or back-end code 
 const MyProjects = () => {
   return (
@@ -9,14 +10,16 @@ const MyProjects = () => {
         My Software Engineering Apps
         </h2>
         </div>
-        <div>
-          <h3>Completed</h3>
-        </div>
-      <div className="body-text">Rural Outfitter</div>
-      <div>
+      <div className="subtitle">Rural Outfitter</div>
+      <div className="body-text">Online clothing store with image carousel and product information module</div>
+      <div className="container">
         <img className="image" src={rural}/>
       </div>
-      <div className="body-text">Cloud B&B</div>
+      <div className="subtitle">Cloud B&B</div>
+      <div className="body-text">Scaled-up legacy codebase to handle 500 requests per second and 10 million primary records in the database</div>
+      <div className="container">
+        <img className="image" src={cloudBNB}/>
+      </div>
         <h3>Under Construction</h3>
       <div className="body-text">Steilacoom Project</div>
       <div className="body-text">Local American Indian Sites Application</div>
